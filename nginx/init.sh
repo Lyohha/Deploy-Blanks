6005ed2f1cfec6e1e94599dd0cd83b66f3f54797
Lyohha <325alex.000@gnail.com>
2025-08-20 22:57:54 +0300

    Update init script

--- nginx/init.sh
+++ nginx/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/Lyohha/Deploy-Blanks/refs/heads/main/nginx/docker-compose.yml ./docker-compose.yml
-wget https://raw.githubusercontent.com/Lyohha/Deploy-Blanks/refs/heads/main/nginx/renew.sh ./renew.sh
+wget https://raw.githubusercontent.com/Lyohha/Deploy-Blanks/refs/heads/main/nginx/docker-compose.yml -O ./docker-compose.yml
+wget https://raw.githubusercontent.com/Lyohha/Deploy-Blanks/refs/heads/main/nginx/renew.sh -O ./renew.sh
 mkdir config
 docker compose up -d
