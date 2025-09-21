9f090fe411cff17a7f4ca36ea78981a4719483b9
Lyohha <325alex.000@gnail.com>
2025-09-21 21:26:22 +0300

    Add insert path to renew certs file

--- nginx/init.sh
+++ nginx/init.sh
@@ -2,5 +2,6 @@
 wget -q https://raw.githubusercontent.com/Lyohha/Deploy-Blanks/refs/heads/main/nginx/docker-compose.yml -O ./docker-compose.yml
 wget -q https://raw.githubusercontent.com/Lyohha/Deploy-Blanks/refs/heads/main/nginx/renew.sh -O ./renew.sh
 chmod +x ./renew.sh
+sed -i "s|FILE|$(pwd)|g" ./renew.sh
 mkdir config
 docker compose up -d
--- nginx/renew.sh
+++ nginx/renew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Date of renew: $(date)"
-docker compose run --remove-orphans certbot renew 
-docker compose exec nginx nginx -s reload
+docker compose -f PATH/docker-compose.yml run --remove-orphans certbot renew 
+docker compose -f PATH/docker-compose.yml exec nginx nginx -s reload
