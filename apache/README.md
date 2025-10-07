0e7e7f04f62f0bc9b47e9f7925a399debf594c31
Lyohha <325alex.000@gnail.com>
2025-10-07 20:23:21 +0300

    Update auto apache user install

---
+++ apache/README.md
@@ -0,0 +1,7 @@
+# Create user for work with Apache
+
+Before using script install Docker
+
+```bash
+bash <(wget -qO- https://raw.githubusercontent.com/Lyohha/Deploy-Blanks/refs/heads/main/apahe/init.sh)
+```
--- apache/init.sh
+++ apache/init.sh
@@ -1,9 +1,19 @@
-wget -q https://raw.githubusercontent.com/Lyohha/Deploy-Blanks/refs/heads/main/apache/docker-compose.yml -O ./docker-compose.yml
-wget -q https://raw.githubusercontent.com/Lyohha/Deploy-Blanks/refs/heads/main/apache/Dockerfile -O ./Dockerfile
-wget -q https://raw.githubusercontent.com/Lyohha/Deploy-Blanks/refs/heads/main/apache/domains.py -O ./domains.py
+#!/bin/bash
+DEPLOY_USER="apache"
+DEPLOY_HOME="/home/$DEPLOY_USER"
+SSH_DIR="$DEPLOY_HOME/.ssh"
+
+sudo useradd -m -d "$DEPLOY_HOME" -s /bin/bash "$DEPLOY_USER"
+
+wget -q https://raw.githubusercontent.com/Lyohha/Deploy-Blanks/refs/heads/main/apache/docker-compose.yml -O "$DEPLOY_HOME/docker-compose.yml"
+wget -q https://raw.githubusercontent.com/Lyohha/Deploy-Blanks/refs/heads/main/apache/Dockerfile -O "$DEPLOY_HOME/Dockerfile"
+wget -q https://raw.githubusercontent.com/Lyohha/Deploy-Blanks/refs/heads/main/apache/domains.py -O "$DEPLOY_HOME/domains.py"
 apt-get install python3-docker
-mkdir config
-mkdir config/php
-mkdir config/apache
-mkdir domains
-docker compose up -d
+mkdir "$DEPLOY_HOME/config"
+mkdir "$DEPLOY_HOME/config/php"
+mkdir "$DEPLOY_HOME/config/apache"
+mkdir "$DEPLOY_HOME/domains"
+
+chown -R $DEPLOY_USER:$DEPLOY_USER $DEPLOY_HOME
+
+passwd $DEPLOY_USER
