a25f38562f3805907a1741c4660c557b32083c80
Lyohha <325alex.000@gnail.com>
2025-10-07 18:00:36 +0300

    Update place for root password in script

--- database/create.sh
+++ database/create.sh
@@ -4,7 +4,7 @@ read -p "DB name: " DB_NAME
 read -s -p "User password: " DB_PASS
 echo
 
-docker exec -i database-mysql81-1 mysql -u root -p <<EOF
+docker exec -i database-mysql81-1 mysql -u root -p<root_password_from_docker-compose.yml_without_spaces> <<EOF
 CREATE DATABASE \`${DB_NAME}\`;
 CREATE USER '${DB_NAME}'@'%' IDENTIFIED BY '${DB_PASS}';
 GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_NAME}'@'%';
